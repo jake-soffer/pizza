a86847a3f0f7518e4c06b414facdf934fd1e9210
jbs <soffer94@gmail.com>
2018-03-30 12:46:48 -0700

    Set up newpizza object.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,40 @@
 // BIZ LOGIC
 
-function pizzaObject(size, toppings, price) {
+function pizzaObject(size, toppings) {
   this.size = size;
-  this.toppings = [];
-  this.price = parseInt("")
+  this.toppings = toppings;
 }
 
-pizzaObject.prototype.toppingsPrice = function() {
-  return this.price += (1.50 * this.toppings.length)
+pizzaObject.prototype.Price = function() {
+  var pizzaSize = this.size;
+  var pizzaToppings = this.toppings.length;
 }
+
+// if (pizzaSize === "small") {
+//   return 12+(pizzaToppings*1.50)
+// } else if (pizzaSize === "medium") {
+//   return 16+(pizzaToppings*1.50)
+// } else if (pizzaSize === "large") {
+//   return 20+(pizzaToppings*1.50)
+// }
 
 // UI LOGIC
 
 $(document).ready(function() {
   $("form#pizza-order").submit(function(event) {
     event.preventDefault();
+    debugger;
     var size = $("input:radio[name=pizza-size]").val();
+    var toppings = [];
     $("input:checkbox[name=pizza-toppings]:checked").each(function() {
-      var toppings = $(this).val();
-      return toppings;
+      toppings.push($(this).val());
     });
-    var newPizza = pizzaObject(size, toppings)
+    var newPizza = new pizzaObject(size, toppings)
 
-    var newPrice = 0;
-
-    pizzaObject.price = newPrice;
-
+    var newPrice = newPizza.Price();
+    $("#order-summary").show();
+    $("#user-size").text(newPizza.size);
+    $("#user-toppings").text(newPizza.toppings)
+    $("#user-price").text(newPrice);
   });
 });
