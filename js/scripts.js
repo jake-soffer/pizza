66a9a373798ede63d64f44495361705086e507ea
jbs <soffer94@gmail.com>
2018-03-30 16:08:02 -0700

    Add more toppings.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,26 @@
 // BIZ LOGIC
 
-function pizzaObject(size, toppings, price) {
+function pizzaObject(size, meats, veggies, price) {
   this.size = size;
-  this.toppings = toppings;
+  this.meats = meats;
+  this.veggies = veggies;
   this.price = price;
 }
 
 pizzaObject.prototype.Price = function() {
   var pizzaSize = this.size;
-  var pizzaToppings = this.toppings.length;
+  var meatToppings = this.meats.length;
+  var veggieToppings = this.veggies.length;
   if (pizzaSize === "Personal") {
-    this.price = (8+(pizzaToppings*1.50));
+    this.price = (8+(meatToppings*2)+(veggieToppings*1.5));
   } else if (pizzaSize === "Small") {
-    this.price = (12+(pizzaToppings*1.50));
+    this.price = (12+(meatToppings*2)+(veggieToppings*1.5));
   } else if (pizzaSize === "Medium") {
-    this.price = (16+(pizzaToppings*1.50));
+    this.price = (16+(meatToppings*2)+(veggieToppings*1.5));
   } else if (pizzaSize === "Large") {
-    this.price = (20+(pizzaToppings*1.50));
+    this.price = (20+(meatToppings*2)+(veggieToppings*1.5));
   } else if (pizzaSize === "Extra Large") {
-    this.price = (24+(pizzaToppings*1.50));
+    this.price = (24+(meatToppings*2)+(veggieToppings*1.5));
   }
 }
 
@@ -27,19 +29,26 @@ pizzaObject.prototype.Price = function() {
 $(document).ready(function() {
   $("form#pizza-order").submit(function(event) {
     event.preventDefault();
-    var size = $("input:radio[name=pizza-size]:checked").val();
-    var toppings = [];
+    var meats = [];
+    var veggies = [];
     var price = 0;
-    $("input:checkbox[name=pizza-toppings]:checked").each(function() {
-      toppings.push($(this).val());
+    var comments = $("input:text").val();
+    var size = $("input:radio[name=pizza-size]:checked").val();
+    $("input:checkbox[name=meat-toppings]:checked").each(function() {
+      meats.push($(this).val());
     });
-    var newPizza = new pizzaObject(size, toppings, price)
+    $("input:checkbox[name=pizza-toppings]:checked").each(function() {
+      veggies.push($(this).val());
+    }); if
+    var newPizza = new pizzaObject(size, meats, veggies, price)
 
     newPizza.Price();
 
     $("#order-summary").show();
     $("#user-size").text(newPizza.size);
-    $("#user-toppings").text(newPizza.toppings)
+    $("#meat-toppings").text(newPizza.meats)
+    $("#veggie-toppings").text(newPizza.veggies)
     $("#user-price").text(newPizza.price);
+    $("#comments").text(comments);
   });
 });
