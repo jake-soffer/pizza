1a19f6636f138070f37486cb8a112fb6c66fe861
jbs <soffer94@gmail.com>
2018-03-30 15:28:05 -0700

    Complete price adjustment functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,41 @@
 // BIZ LOGIC
 
-function pizzaObject(size, toppings) {
+function pizzaObject(size, toppings, price) {
   this.size = size;
   this.toppings = toppings;
+  this.price = price;
 }
 
 pizzaObject.prototype.Price = function() {
   var pizzaSize = this.size;
   var pizzaToppings = this.toppings.length;
+  if (pizzaSize === "Small") {
+    this.price = (12+(pizzaToppings*1.50));
+  } else if (pizzaSize === "Medium") {
+    this.price = (16+(pizzaToppings*1.50));
+  } else if (pizzaSize === "Large") {
+    this.price = (20+(pizzaToppings*1.50));
+  }
 }
-
-// if (pizzaSize === "small") {
-//   return 12+(pizzaToppings*1.50)
-// } else if (pizzaSize === "medium") {
-//   return 16+(pizzaToppings*1.50)
-// } else if (pizzaSize === "large") {
-//   return 20+(pizzaToppings*1.50)
-// }
 
 // UI LOGIC
 
 $(document).ready(function() {
   $("form#pizza-order").submit(function(event) {
     event.preventDefault();
-    debugger;
     var size = $("input:radio[name=pizza-size]").val();
     var toppings = [];
+    var price = 0;
     $("input:checkbox[name=pizza-toppings]:checked").each(function() {
       toppings.push($(this).val());
     });
-    var newPizza = new pizzaObject(size, toppings)
+    var newPizza = new pizzaObject(size, toppings, price)
 
-    var newPrice = newPizza.Price();
+    newPizza.Price();
+
     $("#order-summary").show();
     $("#user-size").text(newPizza.size);
     $("#user-toppings").text(newPizza.toppings)
-    $("#user-price").text(newPrice);
+    $("#user-price").text(newPizza.price);
   });
 });
